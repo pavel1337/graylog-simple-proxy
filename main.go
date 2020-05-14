b0d88680d6d1306d0874d5c0ebfea8f893b09ada
pavel1337 <pavelvoronov1337@gmail.com>
2020-05-15 00:17:12 +0200

    initial dirty hot hack fix whatever

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"strings"
+	"sync"
+
+	"github.com/pavel1337/graylog-simple-proxy/udpnet"
+)
+
+func main() {
+	local := flag.String("l", "localhost:12201", "Local addres string to listen")
+	remote := flag.String("r", "graylog.example.com", "Remote graylog GELF input")
+	insecure := flag.Bool("insecure", false, "Skip Certificate Verification")
+	serverName := flag.String("server-name", "", "Server Name declared in certificate to verificate")
+	ca := flag.String("ca", "", "Certificate Authority path")
+	crt := flag.String("crt", "", "Client certificate path")
+	key := flag.String("key", "", "Client key path")
+	flag.Parse()
+	certPool, pair, err := createCertPool(*ca, *crt, *key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	c := tls.Config{
+		RootCAs:      certPool,
+		Certificates: []tls.Certificate{*pair},
+	}
+	if *insecure {
+		c.InsecureSkipVerify = true
+	}
+	if *serverName != "" {
+		c.ServerName = *serverName
+	}
+
+	ch := make(chan []byte, 100)
+	go sender(ch, *remote, c)
+
+	if strings.Index(*local, "://") == -1 {
+		*local = "udp://" + *local
+	}
+
+	listener, err := udpnet.NewListener(*local)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	go listener.Listen()
+
+	for ret := range listener.Done {
+		switch val := ret.(type) {
+		case *udpnet.FatalError:
+			log.Fatalln(val)
+		case error:
+			log.Println(val)
+		case []byte:
+			id, message := val[:8], val[8:]
+			message = append(message, byte(0))
+			ch <- message
+			fmt.Printf("\n#### %X ####\n\n##########################\n\n", id)
+			// if conn != nil {
+			// 	if c.getBoolVar("new-line") {
+			// 		conn.Push(append(message, '\n'), id)
+			// 	} else {
+			// 		conn.Push(append(message, byte(0)), id)
+			// 	}
+			// }
+		}
+	}
+}
+
+func sender(ch chan []byte, addr string, c tls.Config) {
+	wg := sync.WaitGroup{}
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			for {
+				tlsconn, err := newTlsConnection(addr, &c)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				for m := range ch {
+					tlsconn.Write(m)
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func newTlsConnection(addr string, config *tls.Config) (*tls.Conn, error) {
+	c, err := tls.DialWithDialer(&net.Dialer{}, "tcp", addr, config)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+func createCertPool(ca, crt, pem string) (*x509.CertPool, *tls.Certificate, error) {
+	buf, err := ioutil.ReadFile(ca)
+	if err != nil {
+		return nil, nil, err
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(buf)
+	pair, err := tls.LoadX509KeyPair(crt, pem)
+	if err != nil {
+		return nil, nil, err
+	}
+	return certPool, &pair, err
+
+}
---
+++ udpnet/message.go
@@ -0,0 +1,80 @@
+package udpnet
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type chunk struct {
+	b   []byte
+	sid []byte
+}
+
+type chunkMessage struct {
+	end      time.Time
+	ticker   *time.Ticker
+	chunks   [][]byte
+	listener *Listener
+	id       [8]byte
+	sid      []byte
+	callback func([]byte, error)
+}
+
+func (c *chunkMessage) Close() {
+	c.ticker.Stop()
+}
+
+func (c *chunkMessage) check() {
+	defer c.ticker.Stop()
+	for now := range c.ticker.C {
+		c.listener.lock.Lock()
+
+		if c.valid() {
+			log.Println(fmt.Sprintf("[%X] message %X complete", c.sid, c.id[:]))
+			c.listener.parse(c.merge(), c.sid)
+			delete(c.listener.queue, c.id)
+			c.listener.lock.Unlock()
+			return
+		}
+		// expires, all message should arrive within 5 seconds
+		// http://docs.graylog.org/en/2.3/pages/gelf.html#chunking
+		if c.end.Before(now) {
+			log.Println(fmt.Sprintf("[%X] timeout, discarding message %X", c.sid, c.id[:]))
+			c.listener.lock.Unlock()
+			return
+		}
+		c.listener.lock.Unlock()
+	}
+}
+
+func (c chunkMessage) merge() []byte {
+	var buf []byte
+	for i := 0; i < len(c.chunks); i++ {
+		buf = append(buf, c.chunks[i]...)
+	}
+	return buf
+}
+
+func (c chunkMessage) valid() bool {
+	for i := 0; i < len(c.chunks); i++ {
+		if len(c.chunks[i]) <= 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func NewChunkMessage(ref [][]byte, listener *Listener, id [8]byte, sid []byte) *chunkMessage {
+	message := &chunkMessage{
+		chunks:   ref,
+		sid:      sid,
+		end:      time.Now().Add(5 * time.Second),
+		ticker:   time.NewTicker(250 * time.Millisecond),
+		listener: listener,
+		id:       id,
+	}
+	go message.check()
+	return message
+
+}
